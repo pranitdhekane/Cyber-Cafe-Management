0fd6439518c764ac20a9c52a2b19dafb7f03697d
Pranit Dhekane <49141184+pranitdhekane@users.noreply.github.com>
2026-05-07 09:20:06 +0530

    Update database connection credentials and host

--- ccms/includes/dbconnection.php
+++ ccms/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("db", "root", "root", "ccms");
+$con = mysqli_connect("localhost", "root", "Root@4416", "ccms");
 if (mysqli_connect_errno()) {
   echo "Connection Fail" . mysqli_connect_error();
 }
